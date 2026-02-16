fae4aa2da52a4db3a39fa457888115b4fb31d3b6
N25C73496535462\tanre <249221698+limit-sy@users.noreply.github.com>
2026-02-16 11:17:57 +0900

    サウンド追加

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,12 +16,18 @@ public class GameManager : MonoBehaviour
     public static GameState gameState;
     public string nextSceneName;            // 次のシーン名
 
+    // サウンド関連
+    public AudioClip meGameClear;   // ゲームクリアの音源
+    public AudioClip meGameOver;       // ゲームーバーの音源
+    AudioSource soundPlayer;        // AudioSource型の変数
+
     // スコア追加
     public static int totalScore;   // 合計スコア
 
     void Start()
     {
         gameState = GameState.InGame;
+        soundPlayer = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -29,10 +35,14 @@ public class GameManager : MonoBehaviour
     {
         if (gameState == GameState.GameClear)
         {
+            soundPlayer.Stop(); // ステージ曲を止める
+            soundPlayer.PlayOneShot(meGameClear);   // ゲームクリアの音を1回だけ鳴らす
             gameState = GameState.GameEnd;  // ゲームの状態を更新
         }
         else if (gameState == GameState.GameOver)
         {
+            soundPlayer.Stop(); // ステージ曲を止める
+            soundPlayer.PlayOneShot(meGameOver);   // ゲームオーバーの音を1回だけ鳴らす
             gameState = GameState.GameEnd;  // ゲームの状態を更新
         }
     }
